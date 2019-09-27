f22aec1327424d4a36ffce30d0685836dd71e9ca
hatsu38 <hajiwata0308@gmail.com>
2019-09-27 21:05:29 +0900

    :+1: Initial Commit

---
+++ lib/chemical_elements.rb
@@ -0,0 +1,6 @@
+require "chemical_elements/version"
+
+module ChemicalElements
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/chemical_elements/version.rb
@@ -0,0 +1,3 @@
+module ChemicalElements
+  VERSION = "0.1.0"
+end
