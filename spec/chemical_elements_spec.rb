0efcca6843708ce9233bc8d96bbbd96540d7bd39
hatsu38 <hajiwata0308@gmail.com>
2019-09-28 17:43:54 +0900

    Add RSpec

--- spec/chemical_elements_spec.rb
+++ spec/chemical_elements_spec.rb
@@ -3,7 +3,69 @@ RSpec.describe ChemicalElements do
     expect(ChemicalElements::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  shared_examples 'element is correct' do
+    it { expect(element.name).to eq 'Helium' }
+    it { expect(element.symbol).to eq 'He' }
+    it { expect(element.atomic_num).to eq 2 }
+    it { expect(element.atomic_amount).to eq 4.0026 }
+  end
+
+  describe '.all' do
+    describe 'size' do
+      let(:elements) { ChemicalElements::PeriodicTable.all }
+      it { expect(elements.size).to eq 118 }
+    end
+    describe 'element' do
+      let(:element) { ChemicalElements::PeriodicTable.all[1] }
+      it_behaves_like 'element is correct'
+    end
+  end
+
+  describe '.find' do
+    let(:element) { ChemicalElements::PeriodicTable.find('He') }
+    it_behaves_like 'element is correct'
+  end
+
+  describe '.find_by' do
+    context 'name' do
+      context 'Upcase' do
+        let(:element) { ChemicalElements::PeriodicTable.find_by(name: 'HELIUM') }
+        it_behaves_like 'element is correct'
+      end
+
+      context 'Downcase' do
+        let(:element) { ChemicalElements::PeriodicTable.find_by(name: 'helium') }
+        it_behaves_like 'element is correct'
+      end
+
+      context 'Mix' do
+        let(:element) { ChemicalElements::PeriodicTable.find_by(name: 'HelIum') }
+        it_behaves_like 'element is correct'
+      end
+    end
+    context 'symbol' do
+      context 'Upcase' do
+        let(:element) { ChemicalElements::PeriodicTable.find_by(symbol: 'HE') }
+        it_behaves_like 'element is correct'
+      end
+
+      context 'Downcase' do
+        let(:element) { ChemicalElements::PeriodicTable.find_by(symbol: 'he') }
+        it_behaves_like 'element is correct'
+      end
+
+      context 'Mix' do
+        let(:element) { ChemicalElements::PeriodicTable.find_by(symbol: 'He') }
+        it_behaves_like 'element is correct'
+      end
+    end
+    context 'atomic_num' do
+      let(:element) { ChemicalElements::PeriodicTable.find_by(atomic_num: 2) }
+      it_behaves_like 'element is correct'
+    end
+    context 'atomic_amount' do
+      let(:element) { ChemicalElements::PeriodicTable.find_by(atomic_amount: 4.0026) }
+      it_behaves_like 'element is correct'
+    end
   end
 end
